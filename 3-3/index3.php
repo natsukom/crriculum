c778471cbd3238ff19b68c195f35ef2d42145346
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-12 20:32:02 -0700

    第３章練習問題２
    分からなくなって一応できる範囲は書きましたが添削お願いします。

---
+++ 3-3/index3.php
@@ -0,0 +1,14 @@
+
+<form action="result1.php" method="get">
+０〜９の番号を使って好きな数字の羅列を入力してください。
+<br>
+<input type="number" number="my_number" />
+<input type="submit" value="占う" />
+</form>
+<?php
+$numbers = "1648264038"
+$str = sprit("4");
+$array_rand("6");
+?>
+<?php
+echo date("Y-m-d H:i:s",time());
---
+++ 3-3/result1.php
@@ -0,0 +1,5 @@
+<?php
+$my_number = $_GET['my_number'];
+?>
+<p>の運勢は</p>
+<p>選ばれた数字は</p>
