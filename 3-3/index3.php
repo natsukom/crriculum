631971db8d7c05eaf8ece07596855d5544b803c4
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-13 23:58:39 -0700

    第3章練習問題２

--- 3-3/index3.php
+++ 3-3/index3.php
@@ -2,7 +2,7 @@
 <form action="result1.php" method="get">
 ０〜９の番号を使って好きな数字の羅列を入力してください。
 <br>
-<input type="text" number="my_number" />
+<input type="text" name="my_number" />
 <input type="submit" value="占う" />
 </form>
 
--- 3-3/result1.php
+++ 3-3/result1.php
@@ -1,36 +1,34 @@
 <?php
 $my_number = $_GET['my_number'];
 ?>
-
-
+<p>
 <?php
-$my_number = "0123456789";
-$num_array = str_split($my_number);
-$array = ['0'=>'凶','0~3'=>'小吉','4~6'=>'中吉','7~8'=>'吉','9'=>'大吉'];
-$num_key = array_rand($num_arr,2);
-$result = $num_arr[$num_key];
+echo date("Y/m/d", time());
 ?>
+の運勢は<br>
+
 <?php
-$msg = '小吉'
-if (凶の場合の条件) {
-    $msg = "凶";
+$num_array = str_split($my_number);
+$num_key = array_rand($num_array,1);
+$result = $num_array[$num_key];
+?>
+選ばれた数字は<br>
+<?php echo $result?>
+
+<?php
+if ($num_array ==0) {
     echo "凶です";
-} elseif (小吉の場合の条件) {
-    $msg = "小吉";
+} elseif ($num_array >= 1 and $num_key <= 3) {
     echo "小吉です";
-} elseif (中吉の場合の条件) {
-    $msg = "中吉";
+} elseif ($num_array >= 4 and $num_key <= 6) {
     echo "中吉です";
-} elseif (吉の場合の条件) {
-    $msg = "吉";
+} elseif ($num_array >= 7 and $num_key <= 8) {
     echo "吉です";
 } else {
-    $msg = "大吉";
     echo "大吉です";
 }
 ?>
 <?php
-echo strtotime("1970/8/24 13:31:51");
-<p>の運勢は</p>
-?>
 
+?>
+</p>
