2995ca5e42b22c94b4efaab77bd95a0b7175ff05
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-22 18:26:48 -0700

    第3章チェックテスト

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -1,12 +1,24 @@
-<?php 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>３章チェックテスト</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<?php
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $name = $_POST['name'];
-	$Ans1 = $_POST['Ans1'];
-	$Ans2 = $_POST['Ans2'];
-	$Ans3 = $_POST['Ans3'];
-	$correctAns1 = $_POST['correctAns1'];
-	$correctAns2 = $_POST['correctAns2'];
-	$correctAns3 = $_POST['correctAns3'];
+$Ans1 = $_POST['Ans1'];
+$Ans2 = $_POST['Ans2'];
+$Ans3 = $_POST['Ans3'];
+$correctAns1 = $_POST['correctAns1'];
+$correctAns2 = $_POST['correctAns2'];
+$correctAns3 = $_POST['correctAns3'];
   
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 function checkAnswer($Ans, $correctAns)
@@ -17,16 +29,15 @@ function checkAnswer($Ans, $correctAns)
 			echo "残念・・・";
 	}
 }
-
-<p><?php echo $name ?>yutoさんの結果は・・・？</p>
+?>
+<p><?php echo $name ?>さんの結果は・・・？</p>
 <p>①の答え</p>
-$correctAns1 = ["80","22","20","21"];
-echo $Ans1 = [0]
+<?php checkAnswer( $Ans1, $correctAns1 ); ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-$correctAns2  = ["PHP","Python","JAVA","HTML"];
-echo $Ans2 = [3];
+<?php checkAnswer( $Ans2, $correctAns2 ); ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-$correctAns3  = ["join","select","insert","update"];
-echo $Ans3 = [1];
+<?php checkAnswer( $Ans3, $correctAns3 ); ?>
+</body>
+</html>
--- 3-4/question.php
+++ 3-4/question.php
@@ -1,52 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>３章チェックテスト</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
 <?php
-//POST送信で送られてきた名前を受け取って変数を作成
-$my_name = $_POST['my_name'];
-//①画像を参考に問題文の選択肢の配列を作成してください。
+$name = $_POST['name'];
 
-$question = ["80","22","20","21"];
-$question = ["PHP","Python","JAVA","HTML"];
-$question = ["join","select","insert","update"];
+$q1 = ["80","22","20","21"];
+$q2 = ["PHP","Python","JAVA","HTML"];
+$q3 = ["join","select","insert","update"];
+
+$correctAns1 = $q1[0];
+$correctAns2 = $q2[3];
+$correctAns3 = $q3[1];
 ?>
-//② ①で作成した、配列から正解の選択肢の変数を作成してください
-<?php
-$correctAns1 = $question1[0];
-$correctAns2 = $question2[3];
-$correctAns3 = $question3[1];
-?>
-
-<p>お疲れ様です<!--POST通信で送られてきた名前を出力-->さん</p>
-<p>名前:<?php echo $name ?>yutoさん</p>
-
 
 <!--フォームの作成 通信はPOST通信で-->
-
-
-<h2>①ネットワークのポート番号は何番？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
-<form action="answer.php" method="post">
+	<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+	<p>お疲れ様です<?php echo $name; ?>yutoさん</p>
+	<form action="answer.php" method="post">
 		<h2>①ネットワークのポート番号は何番？</h2>
 		<?php foreach ($q1 as $value1) { ?>
 			<input type="radio" name="Ans1" value="<?php echo $value1;?>">
 			<?php echo $value1; ?>
-
-<h2>②Webページを作成するための言語は？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
-<?php } ?>
+    <?php } ?>
 		<br>
 		<h2>②Webページを作成するための言語は？</h2>
 		<?php foreach ($q2 as $value2) { ?>
 			<input type="radio" name="Ans2" value="<?php echo $value2;?>">
 			<?php echo $value2; ?>
     <?php } ?>
-<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
-<?php foreach ($q3 as $value3) { ?>
+		<br>
+		<h2>③MySQLで情報を取得するためのコマンドは？</h2>
+		<?php foreach ($q3 as $value3) { ?>
 			<input type="radio" name="Ans3" value="<?php echo $value3;?>">
 			<?php echo $value3; ?>
-            <?php } ?>
+    <?php } ?>
 		<br>
-<!--問題の正解の変数と名前の変数を[answer.php]に送る-->
-<input type="hidden" name="name"
+		<input type="hidden" name="name"
 			value="<?php echo $name; ?>" />
 		<input type="hidden" name="correctAns1"
 			value="<?php echo $correctAns1; ?>" />
@@ -56,8 +53,7 @@ $correctAns3 = $question3[1];
 			value="<?php echo $correctAns3; ?>" />
 		<input type="submit" value="回答する" />
 	</form>
-
-
-    
-
+		
+	</body>
+</html>
 
