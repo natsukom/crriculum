3a2652fea1834ac896d4922b6e73a022d2861326
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-24 01:53:12 -0700

    第７章条件分岐・演算子のチャレンジ問題

---
+++ 1-3/main.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 16;
+if (a != b) {
+    console.log(a);
+}
+if (b % 2 === 0) {
+    console.log(b);
+}
+
+let x =10;
+if (x >=20) {
+    console.log('成功です');
+} else {
+console.log('失敗です');
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log('偶数です');
+}
+
+if(y % 2 !== 0){
+console.log('奇数です');
+}
