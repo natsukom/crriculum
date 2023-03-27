236487de4323d344b14da8a6337300fc7ffa9ff2
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-27 00:33:06 -0700

    繰り返し分岐・演算子のチャレンジ問題

---
+++ 1-4/main.js
@@ -0,0 +1,14 @@
+let number = 1;
+while (number <= 100){
+    if(number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz!");
+   
+    }elseif(number % 5 ===0){
+        console.lgg("Buzz!");{
+
+        }elseif(number % 3 ===0){
+            console.lgg("Fuzz!");{
+        }
+number++;
+
+}
