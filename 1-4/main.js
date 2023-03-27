f9babc662743f19cb7bc650db89bd3e38cbd18ee
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-27 01:28:11 -0700

    第7章繰り返し処理チャレンジ問題

--- 1-4/main.js
+++ 1-4/main.js
@@ -1,14 +1,18 @@
 let number = 1;
 while (number <= 100){
     if(number % 3 === 0 && number % 5 === 0){
-        console.log("FizzBuzz!");
-   
-    }elseif(number % 5 ===0){
-        console.lgg("Buzz!");{
+        console.log("FizzBuzz!");{
 
-        }elseif(number % 3 ===0){
-            console.lgg("Fuzz!");{
         }
-number++;
+    
+    }elseif(number % 5 ===0);{
+        console.lgg("Buzz!");{
+        }
+    }
 
-}
+}elseif(number % 3 ===0);{
+            console.lgg("Fuzz!");{
+            }
+number++;
+        }
+
