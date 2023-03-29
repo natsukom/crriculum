4c536c3cd37bb2ea09da75fcdca6fbec8a869da6
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-29 00:41:06 -0700

    第7章配列・オブジェクトチャレンジ問題

---
+++ 1-6/main.js
@@ -0,0 +1,18 @@
+let scores = [10,15,20,25];
+for (let i = 0; i <  scores.length; i++){
+    if(i % 2 == 0){
+console.log(scores[i] + "は" + "偶数です");
+}
+}
+
+
+
+
+let car = {gass: '20.5', num: '1234',};
+
+console.log("ガソリンは" + gass + "です");
+console.log("ナンバーは" + num + "です");
+
+
+
+
