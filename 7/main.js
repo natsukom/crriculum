df8f4a870fc88ecd7f2a0e3b9a5227ce6d5b2997
natsukom <123809372+natsukom@users.noreply.github.com>
2023-03-30 21:14:26 -0700

    第7章チェックテスト

---
+++ 7/main.js
@@ -0,0 +1,25 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+for (let i = 0; i < numbers.length; i++){
+    if(i % 2 == 0){
+ }  
+}
+      
+ function isEven(numbers) {
+    console.log(num[i] + 'は偶数です');
+return numbers + "偶数";
+
+}
+
+class Car {
+    constructor(gas,number){
+        this.gas = gas;
+        this.number = number;
+    }
+
+gasstation() {
+    console.log(`ガソリンは${this.gas}です,ナンバーは${this.number}です`);
+}
+}
+let getNumGas = new Car('getNuwGas','○○','△△');
+getNumGas.gasstation();
